c2ab805a60da880002fcc59ed9f4780200ddad40
EthanMorrell <43864281+EthanMorrell@users.noreply.github.com>
2020-01-31 20:15:23 -1000

    Update incomplete.js

--- incomplete.js
+++ incomplete.js
@@ -1,1 +1,3 @@
-
+// This is where you will paste code that has NOT been fully tested, debugged, or approved by group members.
+// Label the sections that are incomplete with comments.
+// Do not delete these comments.
